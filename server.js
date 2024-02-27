00375f8821dacd9523ccd95eda36b79dedcc5dff
jaiswalvishal1272 <jaiswalvishal1272@gmail.com>
2024-02-27 16:42:36 +0530

    handle cors issue

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const dotenv = require('dotenv').config();
+const cors = require('cors');
 require("./config/db/db.config");
 
 const app = express();
 
-
+app.use(cors());
 
 app.get("/", async(req, resp) => {
     resp.send("app is working fine");
